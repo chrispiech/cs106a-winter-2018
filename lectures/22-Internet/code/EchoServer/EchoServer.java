3504b2b8f1da1187d37bbb8b3034048257ee2816
Christopher Piech <cpiech@stanford.edu>
2018-03-05 16:02:33 -0800

    demo

--- lectures/22-Internet/code/EchoServer/EchoServer.java
+++ lectures/22-Internet/code/EchoServer/EchoServer.java
@@ -1,16 +1,29 @@
 import acm.program.ConsoleProgram;
 
-public class EchoServer extends ConsoleProgram {
+public class EchoServer extends ConsoleProgram implements SimpleServerListener{
 
 	// this makes an object which can receive server requests
 	// it passes those server requests to "this" class.
 	// the second parameter is the port to listen on.
-	private SimpleServer server = null;
+	private SimpleServer server = new SimpleServer(this, 8090);
 	
 	public void run() {
 		setFont("Courier-24");
 		server.start();
 		println("Starting server...");
+	}
+
+	public String requestMade(Request request) {
+		println("I received a request!!!!");
+		String cmd = request.getCommand();
+		if(request.hasParam("secret")) {
+			String secret = request.getParam("secret");
+			println("SHH I got a secret: " + secret);
+		}
+		
+		int n = cmd.length();
+		println("Cmd is: " + cmd);
+		return "The length of the command is: " + n;
 	}
 	
 }
